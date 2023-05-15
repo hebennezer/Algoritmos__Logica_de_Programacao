3b5ff01ca2f5fa760b7909f718516a1c1432fb79
hebennezer <hebennezer@gmail.com>
2023-05-15 21:44:50 +0100

    Create notas.c

---
+++ projetos_c/notas.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    double nota1, nota2, notaFinal;
+
+    printf("Digite a primeira nota: ");
+    scanf("%lf", &nota1);
+
+    printf("Digite a segunda nota:");
+    scanf("%lf", &nota2);
+
+    notaFinal = nota1 + nota2;
+
+    printf("Nota Final = %.1lf\n", notaFinal);
+
+    if (notaFinal < 60.00) {
+        printf("REPROVADO!\n");
+    }
+
+    return 0;
+}
